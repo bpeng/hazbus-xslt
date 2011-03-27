358df88d9342f449c80c3a165f577a6048f3ac9a
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2011-03-27 21:44:15 +0000

    Dummy class to keep build happy.  The project is really XSLTs only.

---
+++ src/nz/org/geonet/esb/event/xslt/Dummy.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nz.org.geonet.esb.event.xslt;
+
+/**
+ *
+ * @author Geoff Clitheroe
+ */
+public class Dummy {
+
+    // Dummy class to keep the build system happy.
+    // At the moment we release a Jar of XSLTs only.
+
+}
